22fb93a6fdc8d40d5e20440922c77372fd937f2b
wanyuaning <wanyuaning@163.com>
2018-12-30 20:10:57 +0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Usage
-npm i @seahan/ewan
+# EWAN CLI
+
+> 构建一切 前端遨游 [https://wmgcuan.github.io/ewan/](https://wmgcuan.github.io/ewan/) 
+
+## 安装说明
+    > npm i @seahan/ewan
+## 启动项目
+    > ewan start <name>
+    
+## 项目
+    > ewan install [name]
+
+## 模板文件
+    > ewan file [type][path]
+
+
+## 框架说明
+####  vue.js 为基础
+####  less 作为CSS的预处理语言，动态的设置页面的尺寸，单位--> @p1 - @p750;  @p1=1rpx=1upx;
+####  mint-ui 作为UI组件库 有丰富的常用组件，如 下拉刷新  上拉加载等功能
+####  animate.css 作为动画库
+####  vuex 作为全局状态管理
+####  axios 作为数据交互的请求库
--- structure/files/index.js
+++ structure/files/index.js
@@ -1,24 +1,12 @@
 var fs = require('fs-extra'),
 	path = require('path'),
-	currentPath = process.cwd(),
-	curring = require('../lib/curring'),
-	package = require('../lib/package')
+	currentPath = process.cwd()
 
 require('../lib/date-format.js')()
 
-module.exports = function(type, savepath) {
-	if (fs.existsSync(__dirname + '/template/' + type)) {
-		curring.step(package.depend, package.create, package.body())(['babel-runtime', '^6.26.0'], true)([
-			'babel-core',
-			'^6.26.4'
-		])(
-			//(["babel-loader", "^7.1.5"])()
-			//(["babel-plugin-transform-runtime", "^6.23.0"])
-			['babel-preset-env', '^1.7.0']
-		)()
-
-		return
-		let targetFile = path.join(currentPath, type)
+module.exports = function(file, savepath) {
+	if (fs.existsSync(__dirname + '/template/' + file)) {
+		let saveTarget = path.join(currentPath, type)
 		//let targetFile = path.join(currentPath, savepath, 'demo-' + type)
 		// if (fs.existsSync(targetFile)) {
 		//   var hash = new Date().format("MMddhhmmss")
